b20f534636ed1b03f8e9712926bc092bfbc78867
taylor296 <tayand.29@gmail.com>
2023-01-25 17:10:44 +0100

    pipeline

--- src/test/java/NumbersCatTest.java
+++ src/test/java/NumbersCatTest.java
@@ -22,7 +22,7 @@ public class NumbersCatTest {
 
     @Test
     public void donat_1_retorna_Un() {
-        assertEquals("Un", NumbersCat.say(1l));
+        assertEquals("Uno", NumbersCat.say(1l));
     }
 
     @Test
